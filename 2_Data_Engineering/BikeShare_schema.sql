4dd02a4f5277c502b5e9908ced7bed5d99816a79
tratnikc <68667850+tratnikc@users.noreply.github.com>
2020-12-14 11:43:19 -0500

    Update BikeShare_schema.sql

--- 2_Data_Engineering/BikeShare_schema.sql
+++ 2_Data_Engineering/BikeShare_schema.sql
@@ -19,4 +19,20 @@ DROP table IF EXISTS Stations CASCADE;
 DROP table IF EXISTS Bikes CASCADE;
 DROP table IF EXISTS Ridership CASCADE;
 
+-- create table Locations
+CREATE table Locations (
+    id int not null,
+    city varchar(100) not null,
+    PRIMARY KEY (id)
+);
+
+CREATE table MemberTypes {
+    id int not null,
+    member_type varchar(50) not null,
+    location_id int,
+    PRIMARY KEY (id)
+    CONSTRAINT fk_membertypes_location
+        FOREIGN KEY (location_id)
+        REFERENCES Locations(location_id)
+}
 
