0511d029d95536d3721f5af8f24c47925f27a121
tratnikc <68667850+tratnikc@users.noreply.github.com>
2020-12-14 14:49:53 -0500

    update ERD

--- 2_Data_Engineering/BikeShare_schema.sql
+++ 2_Data_Engineering/BikeShare_schema.sql
@@ -21,17 +21,17 @@ DROP table IF EXISTS Ridership CASCADE;
 
 -- create table Locations
 CREATE table Locations (
-    id int not null,
+    location_id int not null,
     city varchar(100) not null,
-    PRIMARY KEY (id)
+    country varchar(100) not null,
+    PRIMARY KEY (location_id)
 );
 
 -- create table MemberTypes
 CREATE table MemberTypes (
-    id int not null,
     member_type varchar(50) not null,
-    location_id int,
-    PRIMARY KEY (id)
+    location_id int not null,
+    PRIMARY KEY (member_type, location_id)
     CONSTRAINT fk_membertypes_locations
         FOREIGN KEY (location_id)
         REFERENCES Locations (location_id)
@@ -41,21 +41,17 @@ CREATE table MemberTypes (
 CREATE table Pricing (
     price_id int not null,
     location_id int not null,
-    membertype_id int not null,
+    member_type varchar(50) not null,
     plan varchar(50) not null,
     amount numeric(10,2) not null,
     PRIMARY KEY (price_id),
-    CONSTRAINT fk_pricing_locations
-        FOREIGN KEY (location_id)
-        REFERENCES Locations(location_id),
     CONSTRAINT fk_pricing_membertypes
-        FOREIGN KEY (membertype_id)
-        REFERENCES MemberTypes (id)
+        FOREIGN KEY (member_type, location_id)
+        REFERENCES MemberTypes (member_type, location_id)
 );
 
 -- create table stations
 CREATE table Stations (
-    id int not null,
     station_id int not null,
     station_name varchar(255) not null,
     latitude numeric(11,8) not null,
@@ -69,7 +65,6 @@ CREATE table Stations (
 
 -- create table Bikes
 CREATE table Bikes (
-    id int not null,
     bike_id int not null,
     location_id int not null,
     PRIMARY KEY (bike_id, location_id),
@@ -79,4 +74,27 @@ CREATE table Bikes (
 );
 
 -- create table Ridership
-
+CREATE table Ridership (
+    id int not null,
+    trip_duration int not null,
+    start_station_id int not null,
+    end_station_id int not null,
+    start_date timestamp not null,
+    end_date timestamp not null,
+    bike_id int not null,
+    membertype_id int not null,
+    location_id int not null,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_rides_bikes
+        FOREIGN KEY (bike_id, location_id)
+        REFERENCES Bikes (bike_id, location_id),
+    CONSTRAINT fk_rides_membertype
+        FOREIGN KEY (member_type, location_id)
+        REFERENCES MemberTypes (member_type, location_id),
+    CONSTRAINT fk_rides_stations_start
+        FOREIGN KEY (start_station_id, location_id)
+        REFERENCES Stations (station_id, location_id),
+    CONSTRAINT fk_rides_stations_end
+        FOREIGN KEY (end_station_id, location_id)
+        REFERENCES Stations (station_id, location_id)
+)
